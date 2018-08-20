808215b39a27e08efde100b28c08923af3319ae8
Jim Amsden <jamsden@us.ibm.com>
2018-08-20 15:06:35 -0400

    Added an example content for testing
    There are sample turtle files and curl commands that can be used to create a sample knowledge base with some LDPCs. This can be loaded into an updatable Fuseki store and used to test ldp-service.

---
+++ example/umaine-sparql.sh
@@ -0,0 +1,71 @@
+# Create the University of Maine example data
+#
+# The root LDPC for LDP.js is http://localhost:3000 - everything goes in here.
+#
+
+# Start with the umaine LDP-RS and add it to the root LDPC
+#
+curl --request PUT \
+ --url http://localhost:3030/univ/data?graph=http://localhost:3000/univ/umaine \
+ --header 'Content-Type: text/turtle' \
+ --data-binary @./umaine.ttl \
+ --verbose
+
+# GET the umaine graph that was just created
+curl --request GET \
+ --url http://localhost:3030/univ/data?graph=http://localhost:3000/univ/umaine \
+ --header 'Accept: text/turtle' 
+
+# Create the LDPC to contain the students, this is a new root LDPC
+curl --request PUT \
+ --url http://localhost:3030/univ/data?graph=http://localhost:3000/univ/umaine/students \
+ --header 'Content-Type: text/turtle' \
+ --data-binary @./students.ttl
+
+ # Add Fred Johnson to the students container using LDP
+ curl --request POST\
+ --url http://localhost:3000/umaine/students \
+ --header 'Slug:727175' \
+ --header 'Content-Type:text/turtle' \
+ --data-binary @./727175.ttl \
+ --verbose
+
+  # Add Jay Jackson to the students container
+ curl --request POST \
+ --url http://localhost:3000/umaine/students \
+ --header 'Slug:727188' \
+ --header 'Content-Type:text/turtle' \
+ --data-binary @./727188.ttl
+
+ # Create the LDPC for the teachers
+ curl --request PUT \
+ --url http://localhost:3030/univ/data?graph=http://localhost:3000/univ/umaine/teachers \
+ --header 'Content-Type: text/turtle' \
+ --data-binary @./teachers.ttl
+
+ # Create the LDPC for the courses
+ curl --request PUT \
+ --url http://localhost:3030/univ/data?graph=http://localhost:3000/univ/umaine/courses \
+ --header 'Content-Type: text/turtle' \
+ --data-binary @./courses.ttl
+
+ # Get information about the /umaine/students container
+ curl --request GET \
+ --url http://localhost:3000/univ/umaine/students \
+ --header 'Accept: text/turtle' \
+ --header 'Prefer: return=representation; include="http://www.w3.org/ns/ldp#PreferMinimalContainer"'
+
+ # Get the members of the the /umaine/students container, preferring the containment triples
+ curl --request GET \
+ --url http://localhost:3000/univ/umaine/students \
+ --header 'Accept: text/turtle' \
+ --header 'Prefer: return=representation; include="http://www.w3.org/ns/ldp#PreferContainment http://www.w3.org/ns/ldp#PreferMinimalContainer"'
+
+ # Get the members of the the /umaine/students container, preferring the membership triples
+ curl --request GET \
+ --url http://localhost:3000/univ/umaine/students \
+ --header 'Accept: text/turtle' \
+ --header 'Prefer: return=representation; include="http://www.w3.org/ns/ldp#PreferMembership http://www.w3.org/ns/ldp#PreferMinimalContainer"'
+
+
+
